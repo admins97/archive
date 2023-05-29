6ae866de05f3a04b75db6f784b8b0f6f25ca781e
admins97 <alstjr88@g.skku.edu>
2023-05-29 17:56:20 +0900

    fixed dataset

--- preprocess_txt.py
+++ preprocess_txt.py
@@ -8,28 +8,29 @@ csvData = list(csvReader)
 
 movie_num = []
 under_check = False
+num = ''
 for com in csvData:
     if com[0] == 'movie.num':
         continue
     movie_num.append(int(com[0]))
 
 
-with open("combined_data_1.txt", "r") as f:
+with open("combined_data_4.txt", "r") as f:
     lines = f.readlines()
 
-with open("./new_data_1.txt", "w") as f2:
+with open("./new_data_4.txt", "w") as f2:
     for line in lines:
         # print(line[-2])
         # print(line)
         if line[-2] == ':':
             if int(line.split(':')[0]) in movie_num:
                 under_check = True
-                f2.write(line)
+                num = line.split(':')[0] + ','
             else: 
                 under_check = False
         else:
             if under_check == True:
-                f2.write(line)
+                f2.write(num + line)
             else:
                 continue
                             
