43d2ebd6851edf4e611bf62193086bbd049c1fad
admins97 <alstjr88@g.skku.edu>
2023-05-29 16:17:08 +0900

    preprocessing

---
+++ preprocess.py
@@ -0,0 +1,23 @@
+import csv
+
+csvFile = open('movie_titles.csv','r',newline='',encoding='utf-8')
+csvReader = csv.reader(csvFile)
+print(type(csvReader))
+print(csvReader)
+csvData = list(csvReader)
+
+print(len(csvData))
+newData = []
+newData.append(csvData[0])
+for i in range(1, len(csvData)):
+    if int(csvData[i][0])%2 == 1:
+        newData.append(csvData[i])
+
+print(newData[1][1])
+
+csvFile2 = open('new.csv','w',newline='',encoding='utf-8')
+writer = csv.writer(csvFile2)
+writer.writerows(newData)
+
+csvFile.close()
+csvFile2.close()
